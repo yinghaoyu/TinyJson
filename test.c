c8b53897653d253483c24005573875433bdf82f0
rain <20520@126.com>
2022-08-01 23:49:04 +0800

    add number diagnose

--- test.c
+++ test.c
@@ -79,6 +79,17 @@ static void test_parse_number()
   TEST_NUMBER(1.234E+10, "1.234E+10");
   TEST_NUMBER(1.234E-10, "1.234E-10");
   TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
+
+  TEST_NUMBER(1.0000000000000002, "1.0000000000000002");           /* the smallest number > 1 */
+  TEST_NUMBER(4.9406564584124654e-324, "4.9406564584124654e-324"); /* minimum denorm
+al */
+  TEST_NUMBER(-4.9406564584124654e-324, "-4.9406564584124654e-324");
+  TEST_NUMBER(2.2250738585072009e-308, "2.2250738585072009e-308"); /* Max subnormal double */
+  TEST_NUMBER(-2.2250738585072009e-308, "-2.2250738585072009e-308");
+  TEST_NUMBER(2.2250738585072014e-308, "2.2250738585072014e-308"); /* Min normal positive double */
+  TEST_NUMBER(-2.2250738585072014e-308, "-2.2250738585072014e-308");
+  TEST_NUMBER(1.7976931348623157e+308, "1.7976931348623157e+308"); /* Max double */
+  TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
 }
 
 #define TEST_ERROR(error, json)                  \
@@ -100,7 +111,7 @@ static void test_parse_invalid_value()
 {
   TEST_ERROR(TINY_PARSE_INVALID_VALUE, "nul");
   TEST_ERROR(TINY_PARSE_INVALID_VALUE, "?");
-#if 0
+#if 1
   TEST_ERROR(TINY_PARSE_INVALID_VALUE, "+0");
   TEST_ERROR(TINY_PARSE_INVALID_VALUE, "+1");
   TEST_ERROR(TINY_PARSE_INVALID_VALUE, ".123"); /* at least one digit before '.' */
@@ -115,11 +126,11 @@ static void test_parse_invalid_value()
 static void test_parse_root_not_singular()
 {
   TEST_ERROR(TINY_PARSE_ROOT_NOT_SINGULAR, "null x");
-#if 0
-    /* invalid number */
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "0123"); /* after zero should be '.' , 'E' , 'e' or nothing */
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "0x0");
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "0x123");
+#if 1
+  /* invalid number */
+  TEST_ERROR(TINY_PARSE_ROOT_NOT_SINGULAR, "0123"); /* after zero should be '.' , 'E' , 'e' or nothing */
+  TEST_ERROR(TINY_PARSE_ROOT_NOT_SINGULAR, "0x0");
+  TEST_ERROR(TINY_PARSE_ROOT_NOT_SINGULAR, "0x123");
 #endif
 }
 
--- tinyjson.c
+++ tinyjson.c
@@ -1,6 +1,8 @@
 #include "tinyjson.h"
-#include <assert.h> /* assert() */
-#include <stdlib.h> /* NULL */
+#include <assert.h>  // assert()
+#include <errno.h>   // errno, ERANGE
+#include <math.h>    // HUGE_VAL
+#include <stdlib.h>  // NULL, strtod()
 
 #define EXPECT(c, ch)         \
   do                          \
@@ -8,6 +10,8 @@
     assert(*c->json == (ch)); \
     c->json++;                \
   } while (0)
+#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
 
 typedef struct
 {
@@ -22,48 +26,82 @@ static void tiny_parse_whitespace(tiny_context *c)
   c->json = p;
 }
 
-static int tiny_parse_true(tiny_context *c, tiny_value *v)
+static int tiny_parse_literal(tiny_context *c, tiny_value *v, const char *literal, tiny_type type)
 {
-  EXPECT(c, 't');
-  if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-    return TINY_PARSE_INVALID_VALUE;
-  c->json += 3;
-  v->type = TINY_TRUE;
-  return TINY_PARSE_OK;
-}
-
-static int tiny_parse_false(tiny_context *c, tiny_value *v)
-{
-  EXPECT(c, 'f');
-  if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
-    return TINY_PARSE_INVALID_VALUE;
-  c->json += 4;
-  v->type = TINY_FALSE;
-  return TINY_PARSE_OK;
-}
-
-static int tiny_parse_null(tiny_context *c, tiny_value *v)
-{
-  EXPECT(c, 'n');
-  if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-    return TINY_PARSE_INVALID_VALUE;
-  c->json += 3;
-  v->type = TINY_NULL;
+  size_t i;
+  EXPECT(c, literal[0]);
+  // end with '\0', '\0' ASCII == 0
+  for (i = 0; literal[i + 1]; i++)
+  {
+    if (c->json[i] != literal[i + 1])
+    {
+      return TINY_PARSE_INVALID_VALUE;
+    }
+  }
+  c->json += i;
+  v->type = type;
   return TINY_PARSE_OK;
 }
 
 static int tiny_parse_number(tiny_context *c, tiny_value *v)
 {
-  char *end;
-  /* \TODO validate number */
-  // string to double
-  // return value: number
-  // end: first charatcor cannot convert
-  v->n = strtod(c->json, &end);
-  if (c->json == end)  // cannot convert
-    return TINY_PARSE_INVALID_VALUE;
-  c->json = end;
+  const char *p = c->json;
+  if (*p == '-')  // 负数
+  {
+    p++;
+  }
+  if (*p == '0')  // 只有单个0
+  {
+    p++;
+  }
+  else
+  {
+    // 一个 1-9
+    if (!ISDIGIT1TO9(*p))
+    {
+      return TINY_PARSE_INVALID_VALUE;
+    }
+    // 一个 1-9 再加上任意数量的 digit
+    for (p++; ISDIGIT(*p); p++)
+    {
+    }
+  }
+  if (*p == '.')
+  {
+    p++;
+    // 小数点后至少应有一个 digit
+    if (!ISDIGIT(*p))
+    {
+      return TINY_PARSE_INVALID_VALUE;
+    }
+    for (p++; ISDIGIT(*p); p++)
+    {
+    }
+  }
+  if (*p == 'e' || *p == 'E')
+  {
+    // 有指数部分
+    p++;
+    if (*p == '+' || *p == '-')
+    {
+      p++;
+    }
+    if (!ISDIGIT(*p))
+    {
+      return TINY_PARSE_INVALID_VALUE;
+    }
+    for (p++; ISDIGIT(*p); p++)
+    {
+    }
+  }
+  errno = 0;
+  v->n = strtod(c->json, NULL);
+  if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+  {
+    return TINY_PARSE_NUMBER_TOO_BIG;
+  }
   v->type = TINY_NUMBER;
+  c->json = p;
   return TINY_PARSE_OK;
 }
 
@@ -72,11 +110,11 @@ static int tiny_parse_value(tiny_context *c, tiny_value *v)
   switch (*c->json)
   {
   case 't':
-    return tiny_parse_true(c, v);
+    return tiny_parse_literal(c, v, "true", TINY_TRUE);
   case 'f':
-    return tiny_parse_false(c, v);
+    return tiny_parse_literal(c, v, "false", TINY_FALSE);
   case 'n':
-    return tiny_parse_null(c, v);
+    return tiny_parse_literal(c, v, "null", TINY_NULL);
   case '\0':
     return TINY_PARSE_EXPECT_VALUE;
   default:
@@ -97,6 +135,7 @@ int tiny_parse(tiny_value *v, const char *json)
     tiny_parse_whitespace(&c);
     if (*c.json != '\0')
     {
+      v->type = TINY_NULL;
       ret = TINY_PARSE_ROOT_NOT_SINGULAR;
     }
   }
